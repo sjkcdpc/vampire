8382c0ef7895acb1e35e17fb46eda67b156f6130
zhouxiong <zhouxiong@gaosiedu.com>
2017-07-19 14:56:54 +0800

    use basemapper
    Change-Id: Ieb77ad2902a6d17243fc21c435a7047fce287409

--- src/main/java/com/aixuexi/vampire/util/BaseMapper.java
+++ src/main/java/com/aixuexi/vampire/util/BaseMapper.java
@@ -1,8 +1,15 @@
 package com.aixuexi.vampire.util;
 
+import com.gaosi.api.revolver.model.GoodsOrder;
+import com.gaosi.api.revolver.vo.GoodsOrderVo;
+import ma.glasnost.orika.CustomMapper;
 import ma.glasnost.orika.MapperFactory;
+import ma.glasnost.orika.MappingContext;
 import ma.glasnost.orika.impl.ConfigurableMapper;
+import org.joda.time.DateTime;
 import org.springframework.stereotype.Component;
+
+import java.util.Date;
 
 /**
  * @author zhouxiong
@@ -12,9 +19,17 @@ import org.springframework.stereotype.Component;
 public class BaseMapper extends ConfigurableMapper {
     @Override
     protected void configure(MapperFactory factory) {
-//        factory.classMap(Double.class,Double.class)//TODO
-//                .mapNulls(true).mapNullsInReverse(true)
-//                .byDefault()
-//                .register();
+        factory.classMap(GoodsOrder.class,GoodsOrderVo.class)
+                .mapNulls(true).mapNullsInReverse(true)
+                .byDefault()
+                .customize(new CustomMapper<GoodsOrder, GoodsOrderVo>() {
+                    @Override
+                    public void mapAtoB(GoodsOrder goodsOrder, GoodsOrderVo goodsOrderVo, MappingContext context) {
+                        Date createOrderTime = goodsOrder.getCreateOrderTime();
+                        String createOrderTimeStr = new DateTime(createOrderTime).toString("yyyy-MM-dd HH:mm:ss");
+                        goodsOrderVo.setCreateOrderTime(createOrderTimeStr);
+                    }
+                })
+                .register();
     }
 }
