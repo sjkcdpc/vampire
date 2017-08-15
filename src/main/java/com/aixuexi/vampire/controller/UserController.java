912d260fa4d4117fa9ca52943bbbcb2dc6fc3b5b
wangjing@gaosiedu.com <wangjing@gaosiedu.com>
2017-08-15 17:04:28 +0800

    add filterPermission
    Change-Id: I1e8ffaa7d214489a9d81b6acab21549cb9a284f0

--- src/main/java/com/aixuexi/vampire/controller/UserController.java
+++ src/main/java/com/aixuexi/vampire/controller/UserController.java
@@ -4,11 +4,16 @@ import com.aixuexi.thor.response.ResultData;
 import com.gaosi.api.davincicode.UserService;
 import com.gaosi.api.davincicode.common.service.UserSessionHandler;
 import com.gaosi.api.davincicode.model.User;
+import com.gaosi.api.firstblood.constants.Constant;
+import com.gaosi.api.firstblood.constants.WhitelistEnum;
+import com.gaosi.api.firstblood.model.WhitelistCheck;
+import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -22,6 +27,8 @@ public class UserController {
 
     @Autowired
     private UserService userService;
+    @Resource
+    private com.gaosi.api.independenceDay.service.UserService userServiceIndependenceDay;
 
     /**
      * 用户信息
@@ -40,6 +47,14 @@ public class UserController {
                 result.put("institutionId", user.getInstitutionId());
                 result.put("telephone", user.getTelephone());
                 List<String> permissions = UserSessionHandler.getPermissions();
+                List<Integer> insIds= Lists.newArrayList();
+                insIds.add(user.getInstitutionId());
+                WhitelistCheck whitelistCheck=new WhitelistCheck();
+                whitelistCheck.setWhitelistType(WhitelistEnum.institution);
+                whitelistCheck.setCheckData(insIds);
+                List<WhitelistCheck> whitelistChecks= Lists.newArrayList();
+                whitelistChecks.add(whitelistCheck);
+                permissions=userServiceIndependenceDay.filterPermissions(permissions, Constant.WHITELISTBUSINESS_HEADMASTER, whitelistChecks);
                 Map<String, Integer> menu = new HashMap<>();
                 for (String p : permissions) {
                     menu.put(p, 1);
