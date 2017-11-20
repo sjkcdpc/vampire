70c2f918333cf2af06d050162f824ae36686161f
zhouxiong <zhouxiong@gaosiedu.com>
2017-11-20 12:02:51 +0800

    cg sed
    Change-Id: I4d095368a9ee8c723766aade113904d9b49b7efd

--- sed.sh
+++ sed.sh
@@ -4,10 +4,10 @@ set -e
 #zook=`env | grep zoo | grep 2181 | grep tcp | awk -F'/' '{print $NF}'`
 #sed -i 's#zookeeper.aixuexi.com:2181#'${zook}'#g' /usr/local/tomcat/webapps/ROOT/WEB-INF/classes/config.properties
 #add host
-echo "10.80.85.15 mysql.aixuexi.com" >> /etc/hosts
-echo "10.45.138.45 redis.cluster.aixuexi.com" >> /etc/hosts
-echo "10.45.138.45 redis.aixuexi.com" >> /etc/hosts
-echo "10.45.138.45 ehcache.aixuexi.com" >> /etc/hosts
-echo "10.45.138.45 mongo.aixuexi.com" >> /etc/hosts
-echo "10.45.138.45 memcached.aixuexi.com" >> /etc/hosts
+echo "${IP} mysql.aixuexi.com" >> /etc/hosts
+echo "${IP} redis.cluster.aixuexi.com" >> /etc/hosts
+echo "${IP} redis.aixuexi.com" >> /etc/hosts
+echo "${IP} ehcache.aixuexi.com" >> /etc/hosts
+echo "${IP} mongo.aixuexi.com" >> /etc/hosts
+echo "${IP} memcached.aixuexi.com" >> /etc/hosts
 exec "$@"
