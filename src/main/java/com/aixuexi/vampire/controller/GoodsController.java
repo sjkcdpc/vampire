1e54bff1cc197f9f1c2f0d3c6041f7fbc701c647
ruanyanjie <ruanyanjie@gaosiedu.com>
2018-02-28 16:27:11 +0800

    optimized code
    Change-Id: Ibd88900816e04cfdea42e3d62f81c83112538770

--- src/main/java/com/aixuexi/vampire/controller/GoodsController.java
+++ src/main/java/com/aixuexi/vampire/controller/GoodsController.java
@@ -6,14 +6,16 @@ import com.aixuexi.thor.util.Page;
 import com.aixuexi.vampire.manager.GoodsManager;
 import com.aixuexi.vampire.util.ApiResponseCheck;
 import com.aixuexi.vampire.util.BaseMapper;
-import com.gaosi.api.vulcan.bean.common.BusinessException;
 import com.aixuexi.vampire.util.UserHandleUtil;
 import com.gaosi.api.basicdata.*;
-import com.gaosi.api.basicdata.model.bo.*;
+import com.gaosi.api.basicdata.model.bo.BookVersionBo;
+import com.gaosi.api.basicdata.model.bo.ExamAreaBo;
+import com.gaosi.api.basicdata.model.bo.SchemeBo;
+import com.gaosi.api.basicdata.model.bo.SubjectProductBo;
 import com.gaosi.api.common.constants.ApiRetCode;
 import com.gaosi.api.common.to.ApiResponse;
 import com.gaosi.api.revolver.facade.InvServiceFacade;
-import com.gaosi.api.vulcan.constant.GoodsConstant;
+import com.gaosi.api.vulcan.bean.common.BusinessException;
 import com.gaosi.api.vulcan.facade.GoodsServiceFacade;
 import com.gaosi.api.vulcan.model.GoodsFilterCondition;
 import com.gaosi.api.vulcan.util.CollectionCommonUtil;
@@ -28,7 +30,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.annotation.PostConstruct;
 import javax.annotation.Resource;
 import java.io.UnsupportedEncodingException;
 import java.util.*;
@@ -46,16 +47,10 @@ public class GoodsController {
     private GoodsServiceFacade goodsServiceFacade;
 
     @Resource
-    private SubjectProductApi subjectProductApi;
-
-    @Resource
     private ExamAreaApi examAreaApi;
 
     @Resource
     private BookVersionApi bookVersionApi;
-
-    @Resource
-    private DictionaryApi dictionaryApi;
 
     @Resource
     private SchemeApi schemeApi;
@@ -68,114 +63,6 @@ public class GoodsController {
 
     @Resource(name = "goodsManager")
     private GoodsManager goodsManager;
-
-    /**
-     * 获取学科列表
-     *
-     * @return
-     */
-    @RequestMapping(value = "/getSubject",method = RequestMethod.GET)
-    public ResultData getSubject(){
-        ResultData resultData = new ResultData();
-        ApiResponse<List<Integer>> response = goodsServiceFacade.querySubject();
-
-        //调用获取名字接口
-        List<SubjectProductBo> productBos = subjectProductApi.findSubjectProductListForMall(response.getBody());
-        List<CommonConditionVo> conditionVos = baseMapper.mapAsList(productBos, CommonConditionVo.class);
-        conditionVos.add(0, goodsManager.addAllCondition());
-        resultData.setBody(conditionVos);
-        return resultData;
-    }
-
-    /**
-     * 获取学期列表
-     *
-     * @param subjectId
-     * @return
-     */
-    @RequestMapping(value = "/getPeriod",method = RequestMethod.GET)
-    public ResultData getPeriod(@RequestParam(required = false) Integer subjectId){
-        ResultData resultData = new ResultData();
-        ApiResponse<List<Integer>> response = goodsServiceFacade.queryPeriod(subjectId);
-
-        //需要调用获取名字接口
-        ApiResponse<List<DictionaryBo>> periods = dictionaryApi.findGoodsPeriodByCode(response.getBody());
-        List<DictionaryBo> dictionaryBos = periods.getBody();
-        for(DictionaryBo dictionaryBo :dictionaryBos) {
-            goodsManager.resetPeriodOrder(dictionaryBo);
-        }
-        Collections.sort(dictionaryBos, new Comparator<DictionaryBo>() {
-            @Override
-            public int compare(DictionaryBo o1, DictionaryBo o2) {
-                return o1.getOrderIndex().compareTo(o2.getOrderIndex());
-            }
-        });
-        List<CommonConditionVo> conditionVos = baseMapper.mapAsList(dictionaryBos, CommonConditionVo.class);
-        conditionVos.add(0, goodsManager.addAllCondition());
-        resultData.setBody(conditionVos);
-        return resultData;
-    }
-
-    /**
-     * 获取分类
-     *
-     * @param subjectId
-     * @param periodId
-     * @return
-     */
-    @RequestMapping(value = "/getCategory",method = RequestMethod.GET)
-    public ResultData getCategory(@RequestParam Integer subjectId,
-                                  @RequestParam Integer periodId){
-        ResultData resultData = new ResultData();
-        ApiResponse<List<CommonConditionVo>> response = goodsServiceFacade.queryCategory(subjectId, periodId);
-        List<CommonConditionVo> categoryVos = response.getBody();
-        //解决没有考区和没有教材版本的情况
-        categoryVos.add(goodsManager.addAllCondition());
-        sort(categoryVos);
-        resultData.setBody(categoryVos);
-        return resultData;
-    }
-
-    /**
-     * 获取教材版本或者考区
-     *
-     * @param subjectId
-     * @param periodId
-     * @param categoryId
-     * @return
-     */
-    @RequestMapping(value = "/getBookVersionArea",method = RequestMethod.GET)
-    public ResultData getBookVersionArea(@RequestParam Integer subjectId,
-                                         @RequestParam Integer periodId,
-                                         @RequestParam Integer categoryId){
-        ResultData resultData = new ResultData();
-        List<CommonConditionVo> conditionVos = new ArrayList<>();
-        if (categoryId.equals(GoodsConstant.GoodsCategory.BOOKVERSION.getValue())) {
-            //查询教材版本
-            ApiResponse<List<Integer>> response = goodsServiceFacade.queryBookVersion(subjectId, periodId);
-            ApiResponse<List<BookVersionBo>> bookVersion = bookVersionApi.findByBookVersionIds(response.getBody());
-            if (bookVersion.getRetCode() != ApiRetCode.SUCCESS_CODE || CollectionUtils.isEmpty(bookVersion.getBody())) {
-                logger.error("Get BookVersion Failed ,subjectId :{},periodId : {},categoryId : {}, UserId : {} " +
-                        ",InsId : {}", subjectId, periodId, categoryId, UserHandleUtil.getUserId(), UserHandleUtil.getInsId());
-                throw new BusinessException(ExceptionCode.UNKNOWN, "查询教材版本失败");
-            }
-            conditionVos = baseMapper.mapAsList(bookVersion.getBody(), CommonConditionVo.class);
-        }else if (categoryId.equals(GoodsConstant.GoodsCategory.AREA.getValue())) {
-            //按考区分
-            ApiResponse<List<Integer>> response = goodsServiceFacade.queryArea(subjectId, periodId);
-            ApiResponse<List<ExamAreaBo>> examArea = examAreaApi.findByExamAreaIds(response.getBody());
-            if (examArea.getRetCode() != ApiRetCode.SUCCESS_CODE || CollectionUtils.isEmpty(examArea.getBody())) {
-                logger.error("Get AreaVersion Failed ,subjectId :{},periodId : {},categoryId : {}, UserId : {} " +
-                        ",InsId : {}", subjectId, periodId, categoryId, UserHandleUtil.getUserId(), UserHandleUtil.getInsId());
-                throw new BusinessException(ExceptionCode.UNKNOWN, "查询考区分类失败");
-            }
-            conditionVos = baseMapper.mapAsList(examArea.getBody(), CommonConditionVo.class);
-        }
-        conditionVos.add(goodsManager.addAllCondition());
-        sort(conditionVos);
-        resultData.setBody(conditionVos);
-        return resultData;
-    }
 
     /**
      * 通过商品名模糊查找商品
@@ -194,43 +81,6 @@ public class GoodsController {
         conditionVo.setPageNum(pageNum);
         conditionVo.setPageSize(pageSize);
         conditionVo.setGoodsName(goodName);
-        ApiResponse<Page<GoodsVo>> response = goodsServiceFacade.queryGoodsList(conditionVo);
-        ApiResponseCheck.check(response);
-        Page<GoodsVo> page = response.getBody();
-        dealGoodsVo(Lists.newArrayList(page.getList()));
-        resultData.setBody(page);
-        return resultData;
-    }
-
-    /**
-     * 商品列表查询
-     *
-     * @param sid 学科id
-     * @param pid 学期code
-     * @param vtId 教材版本=1/考区=2
-     * @param veId 教材版本/考区
-     * @param pageNum 当前页
-     * @param pageSize 页大小
-     * @return
-     */
-    @RequestMapping(value = "/goodsList", method = RequestMethod.GET)
-    public ResultData queryGoodsList(@RequestParam Integer sid, @RequestParam Integer pid,
-                                     @RequestParam Integer vtId, @RequestParam Integer veId,
-                                     @RequestParam Integer pageNum, @RequestParam Integer pageSize){
-        ResultData resultData = new ResultData();
-        ReqGoodsConditionVo conditionVo = new ReqGoodsConditionVo();
-        conditionVo.setInstitutionId(UserHandleUtil.getInsId());
-        conditionVo.setSubjectProductId(sid);
-        conditionVo.setPeriodId(pid);
-        conditionVo.setCategoryId(vtId);
-        if(conditionVo.getCategoryId().equals(GoodsConstant.GoodsCategory.BOOKVERSION)) {
-            conditionVo.setBookVersionId(veId);
-        }
-        if(conditionVo.getCategoryId().equals(GoodsConstant.GoodsCategory.AREA)){
-            conditionVo.setExamAreaId(veId);
-        }
-        conditionVo.setPageNum(pageNum);
-        conditionVo.setPageSize(pageSize);
         ApiResponse<Page<GoodsVo>> response = goodsServiceFacade.queryGoodsList(conditionVo);
         ApiResponseCheck.check(response);
         Page<GoodsVo> page = response.getBody();
--- src/main/java/com/aixuexi/vampire/manager/GoodsManager.java
+++ src/main/java/com/aixuexi/vampire/manager/GoodsManager.java
@@ -248,8 +248,16 @@ public class GoodsManager {
     public List<CommonConditionVo> querySubjectProductCondition(List<Integer> subjectProductIds){
         try {
             ImmutableCollection<SubjectProductBo> subjectProductBos = cacheSubjectProduct.getAll(subjectProductIds).values();
+            List<SubjectProductBo> subjectProductBoList = new ArrayList<>(subjectProductBos);
+            // 学科按ID排序
+            Collections.sort(subjectProductBoList, new Comparator<SubjectProductBo>() {
+                @Override
+                public int compare(SubjectProductBo o1, SubjectProductBo o2) {
+                    return o1.getId().compareTo(o2.getId());
+                }
+            });
             // 学科列表
-            List<CommonConditionVo> subjectProducts = baseMapper.mapAsList(subjectProductBos,CommonConditionVo.class);
+            List<CommonConditionVo> subjectProducts = baseMapper.mapAsList(subjectProductBoList,CommonConditionVo.class);
             subjectProducts.add(0, addAllCondition());
             return subjectProducts;
         } catch (Exception e) {
