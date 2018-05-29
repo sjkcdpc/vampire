fa0e9589f818cd9d4ec24ee279d95a76f660f39c
ruanyanjie <ruanyanjie@gaosiedu.com>
2018-05-29 14:47:46 +0800

    add condition
    Change-Id: Ia2962a051c25f82834cd36d919138f0d2012194e

--- src/main/java/com/aixuexi/vampire/manager/OrderManager.java
+++ src/main/java/com/aixuexi/vampire/manager/OrderManager.java
@@ -781,16 +781,18 @@ public class OrderManager {
                 }
             }
             // 处理fieldValue传ID时的展示文案
-            String extInfo = itemOrderVo.getExtInfo();
-            List<TalentTemplateVo> talentTemplateVos = JSONObject.parseArray(extInfo, TalentTemplateVo.class);
-            String key;
-            for (TalentTemplateVo talentTemplateVo : talentTemplateVos) {
-                key = talentTemplateVo.getKey();
-                if(StringUtils.isNotBlank(key)){
-                    talentTemplateVo.setFieldValue(key);
+            if(categoryId.equals(MallItemConstant.Category.RCZX.getId())) {
+                String extInfo = itemOrderVo.getExtInfo();
+                List<TalentTemplateVo> talentTemplateVos = JSONObject.parseArray(extInfo, TalentTemplateVo.class);
+                String key;
+                for (TalentTemplateVo talentTemplateVo : talentTemplateVos) {
+                    key = talentTemplateVo.getKey();
+                    if (StringUtils.isNotBlank(key)) {
+                        talentTemplateVo.setFieldValue(key);
+                    }
                 }
+                itemOrderVo.setExtInfo(JSONObject.toJSONString(talentTemplateVos));
             }
-            itemOrderVo.setExtInfo(JSONObject.toJSONString(talentTemplateVos));
         }
     }
 }
