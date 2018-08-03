11637816c95bcbaa719e38089cc640f3eae0f35e
ruanyanjie <ruanyanjie@gaosiedu.com>
2018-08-03 10:49:45 +0800

    add userId ,insId for exception log
    Change-Id: I6281346f8444df13a722bb6efcb794d6a1c37550

--- src/main/java/com/aixuexi/vampire/controller/GlobalExceptionHandler.java
+++ src/main/java/com/aixuexi/vampire/controller/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.aixuexi.vampire.controller;
 
 import com.aixuexi.thor.response.ResultData;
 
+import com.aixuexi.vampire.util.UserHandleUtil;
 import com.alibaba.fastjson.JSON;
 import com.gaosi.api.vulcan.bean.common.BusinessException;
 import org.slf4j.Logger;
@@ -31,28 +32,32 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(NullPointerException.class)
     @ResponseBody
     public ResultData handleNullPointerException(HttpServletRequest request, NullPointerException e) {
-        logger.error("handleNullPointerException : requestParams:" + JSON.toJSONString(request.getParameterMap()), e);
+        logger.error("handleNullPointerException : requestParams : {} , userId : {}, insId : {} ",
+                JSON.toJSONString(request.getParameterMap()), UserHandleUtil.getUserId(), UserHandleUtil.getInsId(), e);
         return ResultData.failed("操作异常(空)");
     }
 
     @ExceptionHandler(IllegalArgumentException.class)
     @ResponseBody
     public ResultData handleIllegalArgumentException(HttpServletRequest request, IllegalArgumentException e) {
-        logger.error("handleIllegalArgumentException : requestParams:" + JSON.toJSONString(request.getParameterMap()), e);
+        logger.error("handleIllegalArgumentException : requestParams : {} , userId : {}, insId : {} ",
+                JSON.toJSONString(request.getParameterMap()), UserHandleUtil.getUserId(), UserHandleUtil.getInsId(), e);
         return ResultData.failed(e.getMessage());
     }
 
     @ExceptionHandler(InvocationTargetException.class)
     @ResponseBody
     public ResultData handleInvocationTargetException(HttpServletRequest request, InvocationTargetException e) {
-        logger.error("handleInvocationTargetException : requestParams:" + JSON.toJSONString(request.getParameterMap()), e.getTargetException());
+        logger.error("handleInvocationTargetException : requestParams : {} , userId : {}, insId : {} ",
+                JSON.toJSONString(request.getParameterMap()), UserHandleUtil.getUserId(), UserHandleUtil.getInsId(), e.getTargetException());
         return ResultData.failed(e.getTargetException().getMessage());
     }
 
     @ExceptionHandler(Exception.class)
     @ResponseBody
     public ResultData handleException(HttpServletRequest request, Exception e) {
-        logger.error("handleException : requestParams:" + JSON.toJSONString(request.getParameterMap()), e);
+        logger.error("handleException : requestParams : {} , userId : {}, insId : {} ",
+                JSON.toJSONString(request.getParameterMap()), UserHandleUtil.getUserId(), UserHandleUtil.getInsId(), e);
         return ResultData.failed(e.getMessage());
     }
 
