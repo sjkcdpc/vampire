35ba2cea437afd21c0029e80c6a49dd1c1023a61
ruanyanjie <ruanyanjie@gaosiedu.com>
2018-08-20 18:19:50 +0800

    handle exception and goodsTypeVo modify
    Change-Id: Ib08cd86366ebd769442ce47e00eb0c25c2140853

--- src/main/java/com/aixuexi/vampire/controller/GlobalExceptionHandler.java
+++ src/main/java/com/aixuexi/vampire/controller/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package com.aixuexi.vampire.controller;
 import com.aixuexi.thor.response.ResultData;
 
 import com.aixuexi.vampire.util.UserHandleUtil;
+import com.alibaba.dubbo.common.utils.IOUtils;
 import com.alibaba.fastjson.JSON;
 import com.gaosi.api.vulcan.bean.common.BusinessException;
 import org.slf4j.Logger;
@@ -12,6 +13,8 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.lang.reflect.InvocationTargetException;
 
 /**
@@ -24,41 +27,59 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(BusinessException.class)
     @ResponseBody
-    public ResultData handleBusinessException(BusinessException e) {
-        logger.warn("handleBusinessException : " + e.getMessage(), e);
+    public ResultData handleBusinessException(HttpServletRequest request, BusinessException e) {
+        logger.warn("handleBusinessException : requestUrl : {} , requestParams : {} , userId : {}, insId : {} ",
+                request.getRequestURI(), getRequestParameter(request), UserHandleUtil.getUserId(), UserHandleUtil.getInsId(), e);
         return ResultData.failed(e.getMessage());
     }
 
     @ExceptionHandler(NullPointerException.class)
     @ResponseBody
     public ResultData handleNullPointerException(HttpServletRequest request, NullPointerException e) {
-        logger.error("handleNullPointerException : requestParams : {} , userId : {}, insId : {} ",
-                JSON.toJSONString(request.getParameterMap()), UserHandleUtil.getUserId(), UserHandleUtil.getInsId(), e);
+        logger.error("handleNullPointerException : requestUrl : {} , requestParams : {} , userId : {}, insId : {} ",
+                request.getRequestURI(), getRequestParameter(request), UserHandleUtil.getUserId(), UserHandleUtil.getInsId(), e);
         return ResultData.failed("操作异常(空)");
     }
 
     @ExceptionHandler(IllegalArgumentException.class)
     @ResponseBody
     public ResultData handleIllegalArgumentException(HttpServletRequest request, IllegalArgumentException e) {
-        logger.error("handleIllegalArgumentException : requestParams : {} , userId : {}, insId : {} ",
-                JSON.toJSONString(request.getParameterMap()), UserHandleUtil.getUserId(), UserHandleUtil.getInsId(), e);
+        logger.error("handleIllegalArgumentException :  requestUrl : {} , requestParams : {} , userId : {}, insId : {} ",
+                request.getRequestURI(), getRequestParameter(request), UserHandleUtil.getUserId(), UserHandleUtil.getInsId(), e);
         return ResultData.failed(e.getMessage());
     }
 
     @ExceptionHandler(InvocationTargetException.class)
     @ResponseBody
     public ResultData handleInvocationTargetException(HttpServletRequest request, InvocationTargetException e) {
-        logger.error("handleInvocationTargetException : requestParams : {} , userId : {}, insId : {} ",
-                JSON.toJSONString(request.getParameterMap()), UserHandleUtil.getUserId(), UserHandleUtil.getInsId(), e.getTargetException());
+        logger.error("handleInvocationTargetException : requestUrl : {} , requestParams : {} , userId : {}, insId : {} ",
+                request.getRequestURI(), getRequestParameter(request), UserHandleUtil.getUserId(), UserHandleUtil.getInsId(), e.getTargetException());
         return ResultData.failed(e.getTargetException().getMessage());
     }
 
     @ExceptionHandler(Exception.class)
     @ResponseBody
     public ResultData handleException(HttpServletRequest request, Exception e) {
-        logger.error("handleException : requestParams : {} , userId : {}, insId : {} ",
-                JSON.toJSONString(request.getParameterMap()), UserHandleUtil.getUserId(), UserHandleUtil.getInsId(), e);
+        logger.error("handleException : requestUrl : {} , requestParams : {} , userId : {}, insId : {} ",
+                request.getRequestURI(), getRequestParameter(request), UserHandleUtil.getUserId(), UserHandleUtil.getInsId(), e);
         return ResultData.failed(e.getMessage());
+    }
+
+    /**
+     * 获取请求参数
+     * @param request
+     * @return
+     */
+    private String getRequestParameter(HttpServletRequest request) {
+        StringBuilder requestParameter = new StringBuilder();
+        try {
+            requestParameter.append(JSON.toJSONString(request.getParameterMap()));
+            BufferedReader reader = new BufferedReader(new InputStreamReader(request.getInputStream()));
+            requestParameter.append(IOUtils.read(reader));
+        } catch (Exception e) {
+            logger.error("getRequestParameter failed url :{} ", request.getRequestURI(), e);
+        }
+        return requestParameter.toString();
     }
 
 }
---
+++ src/test/java/com/aixuexi/vampire/test/MonitorControllerTest.java
@@ -0,0 +1,18 @@
+package com.aixuexi.vampire.test;
+
+import org.junit.Test;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+/**
+ * Created by ruanyanjie on 2018/8/20.
+ */
+public class MonitorControllerTest extends BaseTest {
+
+    @Test
+    public void getNoLogin() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.
+                get("/monitor/get/nologin")
+        );
+    }
+
+}
