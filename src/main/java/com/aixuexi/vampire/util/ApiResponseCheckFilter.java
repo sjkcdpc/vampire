55fc10f045694a60adb73160c4a030de43109514
ruanyanjie <ruanyanjie@gaosiedu.com>
2018-09-03 11:50:43 +0800

    handle exception
    Change-Id: I6ea35e8473f3418e0ad47f8a42b7a6f2156ae0c4

--- src/main/java/com/aixuexi/vampire/util/ApiResponseCheckFilter.java
+++ src/main/java/com/aixuexi/vampire/util/ApiResponseCheckFilter.java
@@ -12,20 +12,21 @@ public class ApiResponseCheckFilter implements Filter {
     public Result invoke(Invoker<?> invoker, Invocation invocation) throws RpcException {
         Result result = invoker.invoke(invocation);
         Object resultValue = result.getValue();
-        if (result instanceof RpcResult) {
-            RpcResult rpcResult = (RpcResult) result;
-            if (resultValue instanceof ApiResponse){
-                ApiResponse apiResponse =(ApiResponse)resultValue;
-                if (apiResponse.isNotSuccess()){
-                    rpcResult.setException(new RuntimeException(apiResponse.getMessage()));
-                }
-            }else if (resultValue instanceof com.aixuexi.thor.response.ApiResponse){
-                com.aixuexi.thor.response.ApiResponse apiResponse =(com.aixuexi.thor.response.ApiResponse)resultValue;
-                if (apiResponse.isNotSuccess()){
-                    rpcResult.setException(new RuntimeException(apiResponse.getMessage()));
+        if (!result.hasException()) {
+            if (result instanceof RpcResult) {
+                RpcResult rpcResult = (RpcResult) result;
+                if (resultValue instanceof ApiResponse) {
+                    ApiResponse apiResponse = (ApiResponse) resultValue;
+                    if (apiResponse.isNotSuccess()) {
+                        rpcResult.setException(new RuntimeException(apiResponse.getMessage()));
+                    }
+                } else if (resultValue instanceof com.aixuexi.thor.response.ApiResponse) {
+                    com.aixuexi.thor.response.ApiResponse apiResponse = (com.aixuexi.thor.response.ApiResponse) resultValue;
+                    if (apiResponse.isNotSuccess()) {
+                        rpcResult.setException(new RuntimeException(apiResponse.getMessage()));
+                    }
                 }
             }
-
         }
         return result;
     }
