289df11d1bdc769f467986e5348f510de8184b82
ruanyanjie <ruanyanjie@gaosiedu.com>
2018-09-13 11:41:53 +0800

    fix sonar bug ,warn
    Change-Id: Ia8a0849da95edeadd46eb220b211cc4fe267e2e0

--- src/main/java/com/aixuexi/vampire/manager/DictionaryManager.java
+++ src/main/java/com/aixuexi/vampire/manager/DictionaryManager.java
@@ -3,18 +3,21 @@ package com.aixuexi.vampire.manager;
 import com.gaosi.api.basicdata.model.bo.DictionaryBo;
 import com.google.common.collect.Maps;
 import org.apache.commons.collections.CollectionUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.ExecutionException;
 
 /**
  * Created by gaoxinzhong on 2017/6/2.
  */
 @Service("dictionaryManager")
 public class DictionaryManager {
+
+    private static Logger logger = LoggerFactory.getLogger(DictionaryManager.class);
 
     @Resource
     private CacheManager cacheManager;
@@ -71,8 +74,8 @@ public class DictionaryManager {
     public List<DictionaryBo> selectDictByType(final String type) {
         try {
             return cacheManager.getCacheBuilderDict().get(type);
-        } catch (ExecutionException e) {
-            e.printStackTrace();
+        } catch (Exception e) {
+            logger.error("selectDictByType type : {} - catch exception ", type, e);
         }
         return null;
     }
