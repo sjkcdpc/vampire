caf5367e2226d1fcfe3f1d89d80155d1da472585
ruanyanjie <ruanyanjie@gaosiedu.com>
2018-11-22 17:23:22 +0800

    reset version
    Change-Id: Id6a0410f75c391cd6a8d083e35db22d06a72ab1d

--- src/main/java/com/aixuexi/vampire/util/ExpressUtil.java
+++ src/main/java/com/aixuexi/vampire/util/ExpressUtil.java
@@ -1,109 +1,80 @@
 package com.aixuexi.vampire.util;
 
-import com.baidu.disconf.client.common.annotations.DisconfFile;
-import com.baidu.disconf.client.common.annotations.DisconfFileItem;
-import com.baidu.disconf.client.common.annotations.DisconfUpdateService;
-import com.baidu.disconf.client.common.update.IDisconfUpdate;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Scope;
-import org.springframework.stereotype.Component;
-
 /**
  * Created by gaoxinzhong on 2017/5/16.
  */
-@Component("expressUtil")
-@Scope("singleton")
-@DisconfFile(filename = "config.properties")
-@DisconfUpdateService(classes = { ExpressUtil.class })
-public class ExpressUtil implements IDisconfUpdate {
+public class ExpressUtil {
 
-    private final Logger logger = LoggerFactory.getLogger(ExpressUtil.class);
+    /**
+     * 是否走发网
+     */
+    private Boolean syncToWms;
     /**
      * 拆单提示
      */
-    @Value("splitTips")
     private String splitTips;
     /**
      * 时效
      */
-    @Value("aging")
     private String aging;
     /**
      * DIY提示
      */
-    @Value("diyTips")
     private String diyTips;
     /**
      * 发货时间
      */
-    @Value("deliveryTime")
     private String deliveryTime;
     /**
      * 预售发货时间
      */
-    @Value("preSaleDeliveryTime")
     private String preSaleDeliveryTime;
+
+    public Boolean getSyncToWms() {
+        return syncToWms;
+    }
+
+    public void setSyncToWms(Boolean syncToWms) {
+        this.syncToWms = syncToWms;
+    }
+
+    public String getSplitTips() {
+        return splitTips;
+    }
 
     public void setSplitTips(String splitTips) {
         this.splitTips = splitTips;
+    }
+
+    public String getAging() {
+        return aging;
     }
 
     public void setAging(String aging) {
         this.aging = aging;
     }
 
+    public String getDiyTips() {
+        return diyTips;
+    }
+
     public void setDiyTips(String diyTips) {
         this.diyTips = diyTips;
+    }
+
+    public String getDeliveryTime() {
+        return deliveryTime;
     }
 
     public void setDeliveryTime(String deliveryTime) {
         this.deliveryTime = deliveryTime;
     }
 
-    public void setPreSaleDeliveryTime(String preSaleDeliveryTime) {
-        this.preSaleDeliveryTime = preSaleDeliveryTime;
-    }
-
-    @DisconfFileItem(name = "splitTips", associateField = "splitTips")
-    public String getSplitTips() {
-        return splitTips;
-    }
-
-    @DisconfFileItem(name = "aging", associateField = "aging")
-    public String getAging() {
-        return aging;
-    }
-
-    @DisconfFileItem(name = "diyTips", associateField = "diyTips")
-    public String getDiyTips() {
-        return diyTips;
-    }
-
-    @DisconfFileItem(name = "deliveryTime", associateField = "deliveryTime")
-    public String getDeliveryTime() {
-        return deliveryTime;
-    }
-
-    @DisconfFileItem(name = "preSaleDeliveryTime", associateField = "preSaleDeliveryTime")
     public String getPreSaleDeliveryTime() {
         return preSaleDeliveryTime;
     }
 
-    @Override
-    public String toString() {
-        return "ExpressUtil{" +
-                "splitTips='" + splitTips + '\'' +
-                ", aging='" + aging + '\'' +
-                ", diyTips='" + diyTips + '\'' +
-                ", deliveryTime='" + deliveryTime + '\'' +
-                ", preSaleDeliveryTime='" + preSaleDeliveryTime + '\'' +
-                '}';
-    }
-
-    @Override
-    public void reload() throws Exception {
-        logger.info(toString());
+    public void setPreSaleDeliveryTime(String preSaleDeliveryTime) {
+        this.preSaleDeliveryTime = preSaleDeliveryTime;
     }
 }
