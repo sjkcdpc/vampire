5e88b0e3e54b628215185e333e961e0aaa7b9235
liuxinyun <liuxinyun@gaosiedu.com>
2018-12-10 15:45:41 +0800

    fix global exception handler
    Change-Id: I97f6451959aef196d5b9d49a61098f034a1f1e68

--- src/main/java/com/aixuexi/vampire/controller/GlobalExceptionHandler.java
+++ src/main/java/com/aixuexi/vampire/controller/GlobalExceptionHandler.java
@@ -7,9 +7,8 @@ import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import javax.servlet.http.HttpServletRequest;
 import java.io.BufferedReader;
@@ -19,48 +18,29 @@ import java.lang.reflect.InvocationTargetException;
  * @author ruanyanjie
  * @createTime 20171018
  */
-@ControllerAdvice
+@RestControllerAdvice
 public class GlobalExceptionHandler {
     private Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
     @ExceptionHandler(BusinessException.class)
-    @ResponseBody
     public ResultData handleBusinessException(HttpServletRequest request, BusinessException e) {
         logger.warn("handleBusinessException : requestUrl : {} , requestParams : {} , userId : {}, insId : {} , exceptionMsg:{}",
                 request.getRequestURI(), getRequestParameter(request), UserHandleUtil.getUserId(), UserHandleUtil.getInsId(), e.getMessage());
         return ResultData.failed(e.getMessage());
     }
 
-    @ExceptionHandler(NullPointerException.class)
-    @ResponseBody
-    public ResultData handleNullPointerException(HttpServletRequest request, NullPointerException e) {
-        logger.error("handleNullPointerException : requestUrl : {} , requestParams : {} , userId : {}, insId : {} ",
-                request.getRequestURI(), getRequestParameter(request), UserHandleUtil.getUserId(), UserHandleUtil.getInsId(), e);
-        return ResultData.failed("操作异常(空)");
-    }
-
-    @ExceptionHandler(IllegalArgumentException.class)
-    @ResponseBody
-    public ResultData handleIllegalArgumentException(HttpServletRequest request, IllegalArgumentException e) {
-        logger.error("handleIllegalArgumentException :  requestUrl : {} , requestParams : {} , userId : {}, insId : {} ",
-                request.getRequestURI(), getRequestParameter(request), UserHandleUtil.getUserId(), UserHandleUtil.getInsId(), e);
-        return ResultData.failed(e.getMessage());
-    }
-
     @ExceptionHandler(InvocationTargetException.class)
-    @ResponseBody
     public ResultData handleInvocationTargetException(HttpServletRequest request, InvocationTargetException e) {
         logger.error("handleInvocationTargetException : requestUrl : {} , requestParams : {} , userId : {}, insId : {} ",
                 request.getRequestURI(), getRequestParameter(request), UserHandleUtil.getUserId(), UserHandleUtil.getInsId(), e.getTargetException());
-        return ResultData.failed(e.getTargetException().getMessage());
+        return ResultData.failed("服务异常，请联系运营。");
     }
 
     @ExceptionHandler(Exception.class)
-    @ResponseBody
     public ResultData handleException(HttpServletRequest request, Exception e) {
         logger.error("handleException : requestUrl : {} , requestParams : {} , userId : {}, insId : {} ",
                 request.getRequestURI(), getRequestParameter(request), UserHandleUtil.getUserId(), UserHandleUtil.getInsId(), e);
-        return ResultData.failed(e.getMessage());
+        return ResultData.failed("服务异常，请联系运营。");
     }
 
     /**
